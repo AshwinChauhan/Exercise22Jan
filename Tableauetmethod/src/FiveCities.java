ff26b744704659efab40156c7dfab1ece32a5331
ashwin <ashwin@192.168.2.53>
2021-01-22 14:37:39 -0500

    Jan 22 2021

--- Tableauetmethod/src/FiveCities.java
+++ Tableauetmethod/src/FiveCities.java
@@ -9,11 +9,26 @@ public class FiveCities {
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		int []MyTab1= new int[4];
-		for (int i = 0; i < MyTab1.length; i++) 
-		{ MyTab1[i]=sc.nextInt();}
+		int []dist= new int[4];
+		//for loup to input the values of the distances between cities
+		for (int i = 0; i < 4; i++) { dist[i]=sc.nextInt();}
+	
+		int []villes=new int[5];
 		
+		villes[0]=0;
 		
+		for (int i = 1; i < 5; i++) {villes[i]=villes[i-1] + dist[i-1];}
+		
+		for (int i = 0; i < 5; i++) 
+		{
+			for (int j = 0; j < 5; j++) 
+			{
+				int distVille=Math.abs(villes[i]-villes[j]);
+				System.out.print(distVille);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 
 
 	}
